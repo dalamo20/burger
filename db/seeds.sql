80cd0ff4b0ec6cc1ed2cf13db2862bc3db50492f
Daniel A <dalamo90@gmail.com>
2018-05-28 22:21:30 -0500

    connection.js has mysql installed. connection also succeeded.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Green Tea Burger', TRUE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Kimchi Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green Tea Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kimchi Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger', true);
