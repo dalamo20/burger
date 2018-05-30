4004082038703dc07545768869532a7d58e9d803
Daniel A <dalamo90@gmail.com>
2018-05-30 17:14:16 -0500

    double checked connection.js. Server.js is done and has middleware

--- server.js
+++ server.js
@@ -24,40 +24,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Root get route
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) throw err;
-    res.render("index", { burgers: data });
-  });
-});
-
-// Post route -> back to home
-app.post("/", function(req, res) {
-  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, result) {
-    if (err) throw err;
-    res.redirect("/");
-  });
-});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
