6a27fa28d03b9cc2886b7fce35f016911c3c635d
Daniel A <dalamo90@gmail.com>
2018-05-30 20:44:01 -0500

    orm.js f(x))'s defined

--- config/orm.js
+++ config/orm.js
@@ -39,7 +39,7 @@ function printQuestionMarks(num) {
 
 // Object for all our SQL statement functions.
 var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -48,7 +48,7 @@ var orm = {
         cb(result);
       });
     },
-    create: function(table, cols, vals, cb) {
+    insertOne: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
   
       queryString += " (";
@@ -68,7 +68,7 @@ var orm = {
       });
     },
     // An example of objColVals would be {name: panther, sleepy: true}
-    update: function(table, objColVals, condition, cb) {
+    updateOne: function(table, objColVals, condition, cb) {
       var queryString = "UPDATE " + table;
   
       queryString += " SET ";
